0c3bdb9699885602cd890187160c58c481cf680c
Brett McLaughlin <brett.m@me.com>
2014-04-28 19:20:30 -0500

    Adding basic REST service via a single Ruby file.

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ whadru-rest
 ===========
 
 Simple REST API for use by auditions that need to consume data from Whad-R-U-Looking'-At "app"
+
+Requirements
+------------
+
+* Sinatra (http://www.sinatrarb.com)
+* Sinatra::JSON (http://www.sinatrarb.com/contrib/json.html)
+
+Available Endpoints
+-------------------
+
+* / - top-level URI to ensure the API is running. Returns static text string akin to "Hello World"
+* /sites - returns a JSON-formatted list of all sites, including:
+  * the site "key" - identifier for the site, with no spaces
+  * the site information:
+    * site name - human-readable site name
+    * site URL - the full URL of the site
---
+++ whadru-rest.rb
@@ -0,0 +1,69 @@
+require 'sinatra'
+require 'sinatra/json'
+
+set :json_content_type, :js
+
+sites = { 
+  "Earthdata" => { 
+    :name => "NASA Earthdata",
+    :url => "https://earthdata.nasa.gov"
+  },
+  "Element84" => {
+    :name => "Element 84",
+    :url => "http://www.element84.com"
+  },
+  "Sinatra" => {
+    :name => "Sinatra REST Framework",
+    :url => "http://www.sinatrarb.com"
+  },
+  "Reverb" => {
+    :name => "NASA ECHO Reverb",
+    :url => "http://reverb.echo.nasa.gov/"
+  },
+  "EROS" => {
+    :name => "EROS Data Center",
+    :url => "https://eros.usgs.gov"
+  }, 
+  "Marvel" => {
+    :name => "Marvel Comics",
+    :url => "http://marvel.com"
+  },
+  "TaekwonDo" => {
+    :name => "ITF TaeKwon-Do",
+    :url => "http://www.itftkd.org"
+  },
+  "DiveIntoHTML5" => {
+    :name => "Dive Into HTML 5 Informational Site",
+    :url => "http://diveintohtml5.info"
+  },
+  "PennyArcade" => {
+    :name => "Penny Arcade",
+    :url => "http://www.penny-arcade.com"
+  }
+}
+
+get '/' do
+  "Welcome to Whad-R-U-Lookin\'-At. All our datas is for you."
+end
+
+get '/sites' do
+  json :sites => sites
+end
+
+get '/site/:site_key' do |site_key|
+  "Top-level statistics for #{sites[site_key][:name]}"
+end
+
+get '/site/:site_key/name' do |site_key|
+  "#{sites[site_key][:name]}"
+end
+
+get '/site/:site_key/url' do |site_key|
+  "#{sites[site_key][:url]}"
+end
+
+get '/site/:site_key/visitors' do |site_key|
+  numIps = 25 + rand(500) + rand(500)
+  visitors = numIps.times.map { [Array.new(4){rand(256)}.join('.'), 1 + rand(100)] }
+  "#{visitors}"
+end
